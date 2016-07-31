f3d7f8b15138404376cd9f4c11340b501ab0a97e
Constantin Wittenmeier <constantin.wittenmeier@brainsphere.de>
2016-07-31 21:48:11 +0200

    Assignment completed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function represents an Object that can store a matrix and its inverse.
+## The retrunend Object is a list.
+## This list contains a "get"-function, that returns the matrix itself.
+## It also returns a "set"- function, that can be used to set the matrix.
+## When it is used, the inverse of the matric is set to 'NULL'.
+## The inverse has also a "setter" and a "getter"-function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <-NULL
+        set <- function(y){
+                x <<- y
+                inverse <<- NULL
+        }
+        get <-function() x
+        setinverse <- function(matinverse) inverse <<- matinverse
+        getinverse <- function() inverse
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function expects an list that is returned by the makeCacheMatrix-
+## function as an argument. It first checks, if the passed Object does already 
+## contain the inverse. If yes it just returns it. Otherwise it calculates the 
+## inverse and set it in the passed object and returns the calculted inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        if(!is.null(inverse)){
+                message("getting cached inverse")
+                return(inverse)
+        }
+        mat<-x$get()
+        matinverse<-solve(mat)
+        x$setinverse(matinverse)
+        matinverse
 }
